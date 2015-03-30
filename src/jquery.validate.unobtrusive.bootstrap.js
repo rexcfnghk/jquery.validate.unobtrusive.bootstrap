b82843663a40ab6ec0c13a21082ffbd2ae193fe7
Rex Ng <rexng@thisisnoble.com>
2015-03-30 11:25:27 +0800

    Commit source files

---
+++ src/jquery.validate.unobtrusive.bootstrap.js
@@ -0,0 +1,35 @@
+﻿(function($) {
+    var defaultOptions = {
+        errorClass: 'has-error',
+        validClass: 'has-success',
+
+        highlight: function (element, errorClass, validClass) {
+            var $formGroup = $(element).closest('.form-group');
+            $formGroup.addClass(errorClass).removeClass(validClass);
+
+            if ($formGroup.hasClass('has-feedback')) {
+                $formGroup.find('.glyphicon').removeClass('glyphicon-ok').addClass('glyphicon-remove');
+            }
+        },
+        unhighlight: function (element, errorClass, validClass) {
+            var $formGroup = $(element).closest('.form-group');
+            $formGroup.removeClass(errorClass).addClass(validClass);
+
+            if ($formGroup.hasClass('has-feedback')) {
+                $formGroup.find('.glyphicon').removeClass('glyphicon-remove').addClass('glyphicon-ok');
+            }
+        }
+    };
+
+    $.validator.setDefaults(defaultOptions);
+
+    $.validator.unobtrusive.options = {
+        errorClass: defaultOptions.errorClass,
+        validClass: defaultOptions.validClass
+    };
+
+    $.validator.unobtrusive.reparse = $.validator.unobtrusive.reparse|| function(formSelector) {
+        var $form = $(formSelector).removeData('validator').removeData('unobtrusiveValidation');
+        $.validator.unobtrusive.parse($form);
+    };
+}(window.jQuery));
